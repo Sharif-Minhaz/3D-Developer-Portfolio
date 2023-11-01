0245d8e6d40bcc27639e8eaa6f532a9d3e833a7b
Minhaz <sharif35-3001@diu.edu.bd>
2023-11-01 23:00:44 +0600

    styles: padding fixed for input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
 		<div className="xl:mt-12 xl:flex-row flex flex-col-reverse gap-10 overflow-hidden">
 			<motion.div
 				variants={slideIn("left", "tween", 0.2, 1)}
-				className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+				className="flex-[0.75] bg-black-100 xs:p-5 p-8 rounded-2xl"
 			>
 				<p className={styles.sectionSubText}>Get in touch</p>
 				<h3 className={styles.sectionHeadText}>Contact.</h3>
